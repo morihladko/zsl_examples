92df62405c40ff598067dfb812eabd684b749f3d
Martin Babka <babka@atteq.com>
2017-07-27 12:12:21 +0200

    Adapt samples to ZSL 0.17

--- time_tracker/app.py
+++ time_tracker/app.py
@@ -1,4 +1,14 @@
 from time_tracker.time_tracker import app
+from zsl import inject
+
+from zsl.application.modules.web.web_context_module import WebHandler
+
+
+@inject(web_handler=WebHandler)
+def run_web(web_handler):
+    # type:(WebHandler)->None
+    web_handler.run_web(port=5555)
+
 
 if __name__ == "__main__":
-    app.run_web()
+    run_web()
